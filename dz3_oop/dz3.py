609a3b546b2f4b7bf4d59b5c829e06c76e952d31
edonssfall <edonssfall123@gmail.com>
2022-06-23 17:59:31 +0200

    Update dz3.py

--- dz3_oop/dz3.py
+++ dz3_oop/dz3.py
@@ -2,7 +2,7 @@
 
 class Url:
 
-    def __init__(self, scheme = "", authority = "", path = [], query = {}, fragment = []):
+    def __init__(self, scheme = str(), authority = str(), path = list(), query = dict(), fragment = list()):
         self.scheme = scheme
         self.authority = authority
         self.path = path
@@ -13,7 +13,7 @@ class Url:
         return str(self) == str(object)
 
     def __str__(self):
-        text = ""
+        text = str()
         if len(self.scheme) > 0:
             text += f"{self.scheme}://"
         if len(self.authority) > 0:
@@ -21,11 +21,13 @@ class Url:
         if len(self.path) > 0:
             for pathes in self.path:
                 text += f"/{pathes}"
+            self.path.clear()
         if len(self.query) > 0:
             text += "?"
             for key,value in self.query.items():
                 text += f"{key}={value}&"
             text = text[:-1]
+            self.query.clear()
         if len(self.fragment) > 0:
             text += "#"
             for fragments in self.fragment:
@@ -34,27 +36,27 @@ class Url:
 
 class HttpUrl(Url):
 
-    def __init__(self, scheme="http", authority="", path=[], query={}, fragment=""):
+    def __init__(self, scheme="http", authority=str(), path=list(), query=dict(), fragment=list()):
         super().__init__(scheme, authority, path, query, fragment)
 
 class HttpsUrl(Url):
 
-    def __init__(self, scheme="https", authority="", path=[], query={}, fragment=""):
+    def __init__(self, scheme="https", authority=str(), path=list(), query=dict(), fragment=list()):
         super().__init__(scheme, authority, path, query, fragment)
 
 class GoogleUrl(HttpsUrl):
 
-    def __init__(self, scheme="https", authority="google.com", path=[], query={}, fragment=""):
+    def __init__(self, scheme="https", authority="google.com", path=list(), query=dict(), fragment=list()):
         super().__init__(scheme, authority, path, query, fragment)
 
 class WikiUrl(HttpsUrl):
 
-    def __init__(self, scheme="https", authority="wikipedia.org", path=[], query={}, fragment=""):
+    def __init__(self, scheme="https", authority="wikipedia.org", path=list(), query=dict(), fragment=list()):
         super().__init__(scheme, authority, path, query, fragment)
 
 class UrlCreator(Url):
-
-    def __init__(self, scheme="", authority="", path=[], query={}, fragment=[]):
+    path_list = []
+    def __init__(self, scheme=str(), authority=str(), path=list(), query=dict(), fragment=list()):
         super().__init__(scheme, authority, path, query, fragment)
 
     def _create(self):
@@ -65,13 +67,9 @@ class UrlCreator(Url):
             self.path.append(arg)
         for key, value in kwds.items():
             self.query.update({key: value})
+        return UrlCreator(self.scheme, self.authority, self.path, self.query)
             
     def __getattr__(self, name):
         self.path.append(name)
+        return UrlCreator(self.scheme, self.authority, self.path)
 
-url_creator = UrlCreator(scheme='https', authority='docs.python.org')
-url_creator.gfd
-url_creator.dgd
-url_creator.sfsr
-
-print(url_creator)
